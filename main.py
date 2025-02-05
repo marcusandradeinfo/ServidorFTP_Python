1f1373f4fa2b58cfdfe1dea28dcd908456f1a283
Marcus Andrade <profmarcusandrade@gmail.com>
2025-02-05 20:23:41 -0300

    Validação de botos do front end integrados com o back end

--- main.py
+++ main.py
@@ -1,46 +1,62 @@
 import streamlit as st 
-from Controller import controllerbuttons
+import time
+from Controller import servidor
+
 
 def Main():
+    server =  servidor.ServerFtp()
     st.set_page_config(page_title=None, page_icon=None, layout="wide", initial_sidebar_state="collapsed", menu_items=None)
     st.text("SERVIDOR FTP")
     st.divider()
-
-    ### Criação das variáveis de Sessão para controle da Página
     if "txtip" not in st.session_state:
-        st.session_state.txtip = False
+         st.session_state.txtip = False
+    if "log" not in st.session_state:
+        st.session_state.log = ""
+    if "numip" not in st.session_state:
+        st.session_state.numip = None
 
 
     with st.container(height=100):
-        teste, teste2, teste3 = st.columns([1, 0.5, 4], vertical_alignment='top')
-        ##### st.sessionstate.txtip será habilitada ou não
-        teste.text_input("Insira o IP do Servidor", disabled=st.session_state.txtip)
-        dados_porta = teste2.text_input("Porta")
+        teste,teste2,teste3 = st.columns([1,0.5,4],vertical_alignment='top')
+        result_ip = teste.text_input("Insira o IP do Servidor",disabled=st.session_state.txtip)
+        result_porta = teste2.text_input("Porta")
     btn_buscar_ip = st.button("Buscar IP")
     if btn_buscar_ip:
-        result_ip = controllerbuttons.BuscarIP()
-        if result_ip is not None and dados_porta != "":
-            print(f"Dados da variável porta: {dados_porta}")
-            st.success(f'Endereço de IP: {result_ip} obtido com exito !', icon="✅")
+        result_ip = server.obter_ip()
+        if result_ip is not None:
             st.session_state.txtip = True
-            st.rerun()
-        else:
-             st.error("Favor inserir uma Porta para o Servidor", icon="🚨")
-             st.session_state.txtip = False
-             st.rerun()
-    txtusuario , txtsenha = st.columns([1,1])
-    with txtusuario:
-        st.text_input("Usuário")
-    with txtsenha:
-        st.text_input("Senha")
-    btn_iniciar, btn_pausar, col_espaco = st.columns([1,1,6])
-    with btn_iniciar:
-        st.button("Iniciar Servidor")
-    with btn_pausar:
-        st.button("Pausar Servidor")
+            print(f"result ip: {result_ip}")
+            st.session_state.numip = result_ip
+            print(f"result ip: {st.session_state.numip}")
+            st.success(f'Endereço de IP: {result_ip} obtido com exito !', icon="✅")
+            time.sleep(2)
+            # st.rerun()
+            print(f"result ip: {st.session_state.numip}")
+
+    coltxtusuario , coltxtsenha = st.columns([1,1])
+    with coltxtusuario:
+        txtuser = st.text_input("Usuário")
+    with coltxtsenha:
+        txtsenha = st.text_input("Senha")
+    col_btn_iniciar, col_btn_pausar, col_espaco = st.columns([1,1,6])
+    with col_btn_iniciar:
+        btn_iniciar = st.button("Iniciar Servidor")
+        if btn_iniciar:
+            print(st.session_state.numip)
+            if st.session_state.numip is not None and st.session_state.numip != "":
+                if result_porta != "" and txtuser != "" and txtsenha != "":
+                    
+                    st.session_state.log = "Servidor Iniciado"
+
+                else:
+                    st.session_state.log = "Servidor não pode ser iniciado erro na porta, usuário ou senha"
+            else:
+                st.session_state.log ="Servidor não iniciado favor verificar número de IP"
+    with col_btn_pausar:
+        btn_pausar = st.button("Pausar Servidor")
     with col_espaco:
         pass
     st.divider()
-    st.text_area("Log Servidor")
+    st.text_area("Log Servidor",value=st.session_state.log)
 if __name__ == "__main__":
     Main()
